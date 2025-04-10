ef9cc207be422dad479d5cfdcada7e257ceb5e86
DeWOpQ <114812485+DeWOpQ@users.noreply.github.com>
2025-04-10 23:36:29 +0530

    commit to main

---
+++ frontend/build/static/js/198.43aa4411.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmines_frontend=self.webpackChunkmines_frontend||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.43aa4411.chunk.js.map
